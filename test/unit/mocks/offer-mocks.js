0aa285c13acf19775b50d3f57a13a8e65978f439
Chris Troutner <chris.troutner@gmail.com>
2022-10-02 17:07:48 -0700

    Starting take library

---
+++ test/unit/mocks/offer-mocks.js
@@ -0,0 +1,53 @@
+
+const simpleNftOffer01 = {
+  nsfw: false,
+  flags: [],
+  _id: '6321d7d3b3b07c0013e05291',
+  messageType: 1,
+  messageClass: 1,
+  tokenId: '60bf61798028d7f7ddb6e887ad5a0b03ae2129e4a406e5763dd6c1e23652e6f1',
+  buyOrSell: 'sell',
+  rateInBaseUnit: '84125',
+  minUnitsToExchange: '84125',
+  numTokens: 1,
+  utxoTxid: '59c94d6498aaeb8b761dc608eab63e93cccba571978c475df3f661e61587a400',
+  utxoVout: 1,
+  timestamp: 1663162278523,
+  localTimestamp: '9/14/2022, 1:31:18 PM',
+  p2wdbHash: 'zdpuApmP8rKL7Eo7CU7qJKCD1nUzVwtudQzQmzMDUiie6AQBn',
+  offerStatus: 'posted',
+  makerAddr: 'bitcoincash:qrqlz63cwmu0hcmsrfnd8jemn3atkpaqds6tf4ksrr',
+  ticker: 'CTAIA002',
+  tokenType: 1,
+  displayCategory: 'simple-nft',
+  __v: 0
+}
+
+const nftOffer01 = {
+  nsfw: false,
+  flags: [],
+  _id: '63212842b3b07c0013e05154',
+  messageType: 1,
+  messageClass: 1,
+  tokenId: 'eb93f05553ff088bffb0ec687519e83c59e5108c160f7c25a4b6c45109d7e40b',
+  buyOrSell: 'sell',
+  rateInBaseUnit: '8488964',
+  minUnitsToExchange: '8488964',
+  numTokens: 1,
+  utxoTxid: 'c39b5f77d266170a8b1633f58e8cf9d310f03f250b3e454293b03b85bbd17b79',
+  utxoVout: 1,
+  timestamp: 1663117330642,
+  localTimestamp: '9/14/2022, 1:02:10 AM',
+  p2wdbHash: 'zdpuAvRRYyN2KZdB8kKfssQyGSVo9zsWhmD3oWtDobLJhiF6m',
+  offerStatus: 'posted',
+  makerAddr: 'bitcoincash:qrqlz63cwmu0hcmsrfnd8jemn3atkpaqds6tf4ksrr',
+  ticker: 'TV001',
+  tokenType: 65,
+  displayCategory: 'nft',
+  __v: 0
+}
+
+module.exports = {
+  simpleNftOffer01,
+  nftOffer01
+}
